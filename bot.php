17dcbe8096cda12f072c133edd53efeaa433d12c
Dmitry Bazhin <homopoluza@gmail.com>
2023-04-16 12:45:35 +0600

    init

---
+++ bot.php
@@ -0,0 +1,43 @@
+<?php
+$path = "https://api.telegram.org/bot5849536509:AAHzOfwJ04kAL-cyu40UQDKJSiNwOwrYCLQ";
+$update = json_decode(file_get_contents("php://input"), TRUE);
+
+// User's info
+$chatId = $update["message"]["chat"]["id"];
+$message = $update["message"]["text"];
+$name = $update["message"]["chat"]["first_name"];
+$messageID = $update["message"]["message_id"];
+$userID = $update["message"]["chat"]["id"];
+
+$askMeWeather = $path."/sendmessage?chat_id=".$chatId.$name."&text=You can ask me about the weather. Just type: /weather <city_name>";
+
+$data = file_get_contents("id.txt");
+if (strpos($data, $userID) === false){
+    file_put_contents("id.txt", $userID.PHP_EOL, FILE_APPEND);
+    $greeting = $path."/sendmessage?chat_id=".$chatId."&text=Hi ".$name;
+    $analyzing = $path."/sendmessage?chat_id=".$chatId."&text=Analyzing user's profile";
+    $dots = $path."/sendmessage?chat_id=".$chatId."&text=...";
+    $gay = $path."/sendmessage?chat_id=".$chatId.$name."&text=You are so gay!";
+
+    file_get_contents($greeting);
+    file_get_contents($analyzing);
+    file_get_contents($dots);
+    sleep(2);
+    file_get_contents($gay);
+
+    // json
+    $json = json_encode($update, JSON_PRETTY_PRINT);
+    $file = fopen("./$name.json",'w');
+    fwrite($file,$json);
+    fclose($file); 
+}
+
+
+if (strpos($message, "/weather") === 0) {
+$location = substr($message, 9);
+$weather = json_decode(file_get_contents("https://api.openweathermap.org/data/2.5/weather?q=".$location."&appid=3cd9ad621832ffbc3b9831c4f16721c0"), TRUE)["weather"][0]["main"];
+file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Here's the weather in ".$location.": ". $weather);
+} else {
+    file_get_contents($askMeWeather);
+}
+?>
